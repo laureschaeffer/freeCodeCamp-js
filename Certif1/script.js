4d247791bca32ab99ed9e07c232d32084c2fe118
laureschaeffer <laure.schaeffer@hotmail.com>
2024-05-12 18:17:03 +0200

    start of the exercice about football team cards

--- Certif1/script.js
+++ Certif1/script.js
@@ -10,28 +10,36 @@ checkBtn.addEventListener('click', compareStr);
 //clean the input then compare 2 strings
 function compareStr() {
     var inputValue = txtInput.value;
-    //regex that only allow letters and numbers
-    const regex = /[^a-zA-Z0-9]/g;
-    // const regex = /[\W_]/g ;
-    // replace every unwanted characters and give only lowercase
-    var stringCleaned = inputValue.replace(regex, '');
-    var stringLow = stringCleaned.toLowerCase();
-    console.log(stringLow)
-    
-    //get the inversed string
-    // The split() method divides the string into an array of characters, reverse() reverses the array, and join() combines the reversed characters into a new string, effectively reversing the original string
-    var strRev =  stringLow.split('').reverse().join('');
 
-    //check if they both match
-    var output =""
-    if(stringLow == strRev){
-        output = " is a palindrome";
+    //check if it's not empty
+    if(inputValue === ""){
+        alert("Please input a value");
     } else {
-        output = " is not a palindrome";
-    }
 
-    //add to the dom the result
-    return result.innerHTML = `<p>"${inputValue}" ${output}</p>`
+        //regex that only allow letters and numbers
+        const regex = /[^a-zA-Z0-9]/g;
+        // const regex = /[\W_]/g ;
+        // replace every unwanted characters and give only lowercase
+        var stringCleaned = inputValue.replace(regex, '');
+        var stringLow = stringCleaned.toLowerCase();
+        console.log(stringLow)
+    
+        //get the inversed string
+        // The split() method divides the string into an array of characters, reverse() reverses the array, and join() combines the reversed characters into a new string, effectively reversing the original string
+        var strRev =  stringLow.split('').reverse().join('');
+    
+        //check if they both match
+        var output =""
+        if(stringLow == strRev){
+            output = " is a palindrome";
+        } else {
+            output = " is not a palindrome";
+        }
+    
+        //add to the dom the result
+        return result.innerHTML = `<p>"${inputValue}" ${output}</p>`
+
+    }
   }
 
 
