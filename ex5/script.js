091f12e23ae95ecf9b2c1092edfc191ac02a05be
laureschaeffer <laure.schaeffer@hotmail.com>
2024-05-21 20:55:41 +0200

    progress on the card function

--- ex5/script.js
+++ ex5/script.js
@@ -32,10 +32,22 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
   const taskObj = {
-
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
 
-});
+   if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(({id, title, date, description}) => {
+      (tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+        </div>
+        `)
+      }
+    );
+  });
